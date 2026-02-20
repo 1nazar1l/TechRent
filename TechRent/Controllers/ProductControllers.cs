f621ff9b5f0d8aef16e55e67044931bad5356afc
nazar <nazarvusik77@gmail.com>
2026-02-20 18:52:04 +0300

    Добавил страницу для товара

---
+++ TechRent/Controllers/ProductControllers.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechRent.Data;
+using TechRent.Models.Entities;
+
+namespace TechRent.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipment
+        public async Task<IActionResult> Index()
+        {
+            var equipment = await _context.Equipments.ToListAsync();
+            return View(equipment);
+        }
+    }
+}
